711232f05596033c234910d70300248645ea6eb7
James Azarja <jazarja@yahoo.com>
2022-06-18 09:47:29 +0000

    Update gitpod init

--- tests/index.ts
+++ tests/index.ts
@@ -16,15 +16,15 @@ const HARD_MIGRATION_ENABLED = true
 const knexConfig = {
     client: 'mysql',
     connection: {
-    "host": "localhost",
-    "user": "test",
-    "password": "test",
-    "database": "test",
-    "ssl": {
-      "rejectUnauthorized": "true",
-      "secureProtocol": "TLSv1_2_method"
+        "host": "localhost",
+        "user": "root",
+        "password": "testroot",
+        "database": "test",
+        // "ssl": {
+        //   "rejectUnauthorized": "true",
+        //   "secureProtocol": "TLSv1_2_method"
+        // }
     }
-}
 }
 
 const knexInstance = knex(knexConfig);
@@ -39,7 +39,7 @@ const main = async () => {
     describe('Table-Engine errors.', () => TABLE_ENGINE_ERRORS_ENABLED && require('./table-engine-errors'))
     describe(`Table-Engine GENERAL defaut value errors.`, () => GENERATE_DEFAULT_VALUE_ERRORS_ENABLED && require('./general-default-value-errors'))
     describe(`Table-Engine NUMBER defaut value errors.`, () => NUMBER_DEFAULT_VALUE_ENABLED && require('./number-default-value-errors'))
-    describe(`Table-Engine STRING defaut value errors.`, () => STRING_DEFAULT_VALUE_ENABLED && require('./string-default-value-errors'))    
+    describe(`Table-Engine STRING defaut value errors.`, () => STRING_DEFAULT_VALUE_ENABLED && require('./string-default-value-errors'))
     describe(`Table-Engine DATE defaut value errors.`, () => DATE_DEFAULT_VALUE_ENABLED && require('./date-default-value-errors'))
     describe('Connected tests with MySQL server', () => MYSQL_CONNECTED_ENABLED && require('./mysql-connected'))
     describe('Ecosystem methods', () => ECOSYSTEM_ENABLED && require('./ecosystem'))
